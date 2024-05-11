00bead7117cb99400cee91a3575d299ce0896e04
fengzuyan <fengzuyan@itbox.cn>
2024-05-11 17:35:32 +0800

    代码优化：透明弹窗处理

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -88,6 +88,7 @@ class _MyAppState extends State<MyApp> {
       onGenerateRoute: _generateRoute,
       navigatorKey: rootKey,
       initialRoute: "/",
+      theme: ThemeData.light(),
       onGenerateInitialRoutes: (String initialRoute) {
         print('initialRoute: $initialRoute');
         HzNavigator.rootRoute = initialRoute;
