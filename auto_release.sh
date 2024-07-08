749ca3f9c25c3d09cdf162d100027896a5e49998
dongjianxiong <dongjianxiong@itbox.cn>
2024-07-08 13:52:47 +0800

    更新发布脚本

--- auto_release.sh
+++ auto_release.sh
@@ -5,17 +5,16 @@ file=pubspec.yaml
 
 # 读取文件中的版本号
 version_line=$(grep 'version:' $file)
-current_version=$(echo $version_line | sed 's/version: //')
-echo "current version is: $current_version"
+# shellcheck disable=SC2001
+current_version=$(echo "$version_line" | sed 's/version: //')
+echo "当前版本: $current_version"
 # 分割版本号
-#base_version=$(echo $current_version | cut -d'+' -f1)
-#build_number=$(echo $current_version | cut -d'+' -f2)
-head_version=$(echo $current_version | cut -d'.' -f1)
-mid_version=$(echo $current_version | cut -d'.' -f2)
-last_version=$(echo $current_version | cut -d'.' -f3)
-echo "head_version:$head_version, mid_version: $mid_version, last_version:$last_version"
+head_version=$(echo "$current_version" | cut -d'.' -f1)
+mid_version=$(echo "$current_version" | cut -d'.' -f2)
+last_version=$(echo "$current_version" | cut -d'.' -f3)
 # 自增版本号
 type=$1
+echo "自增版本类型： $type"
 if [ "$type" == '1' ]; then
   mid_version=$((mid_version + 1))
 elif [ "$type" == '2' ]; then
@@ -25,15 +24,17 @@ else
   last_version=$((last_version + 1))
 fi
 new_version="${head_version}.${mid_version}.${last_version}"
-echo "new version is: $new_version"
 # 替换文件中的版本号
 sed -i '' "s/version: $current_version/version: $new_version/" $file
 
 # 升级版本号
+echo "升级版本号： $new_version"
 git add .
 git commit -m "feat: 升级版本号"
+echo "开始push新的提交"
 git push
 
 # 打tag
+echo "开始打tag"
 git tag $new_version
 git push origin $new_version
