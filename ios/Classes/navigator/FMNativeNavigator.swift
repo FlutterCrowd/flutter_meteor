da368ff2392dd20130d83ec686cf33d223af5ff9
dongjianxiong <dongjianxiong@itbox.cn>
2024-07-24 09:56:24 +0800

    fix: 回复多删除的代码

---
+++ ios/Classes/navigator/FMNativeNavigator.swift
@@ -0,0 +1,105 @@
+//
+//  HzNativeNavigator.swift
+//  hz_router
+//
+//  Created by itbox_djx on 2024/5/10.
+//
+
+import UIKit
+
+public class FMNativeNavigator: NSObject {
+    
+    static public func present(toPage: UIViewController) {
+        topViewController()?.present(toPage, animated: true)
+    }
+    
+    static public func push(toPage: UIViewController) {
+        topViewController()?.navigationController?.pushViewController(toPage, animated: true)
+    }
+    
+    static public func pop() {
+        dismissOrPop(animated: true)
+    }
+    
+    static public func dismiss() {
+        dismissOrPop(animated: true)
+    }
+    
+    static public func popUntil(untilPage: UIViewController) {
+        if let navigationController = topViewController()?.navigationController {
+            if (navigationController.viewControllers.contains(untilPage)) {
+                navigationController.popToViewController(untilPage, animated: true)
+
+            } else {
+                navigationController.dismiss(animated: false) {
+                    popUntil(untilPage: untilPage)
+                }
+            }
+        }
+    }
+    
+    static public func popToRoot() {
+        
+        if FMNavigatorObserver.rootViewController()?.presentedViewController == nil {
+            topViewController()?.navigationController?.popToRootViewController(animated: true)
+        } else {
+            topViewController()?.dismiss(animated: false, completion: {
+                popToRoot()
+            })
+        }
+    }
+    
+    static public func pushToReplacement(toPage: UIViewController) {
+        if let navigationController = topViewController()?.navigationController {
+            if (navigationController.viewControllers.count > 0) {
+                navigationController.popViewController(animated: false)
+                navigationController.pushViewController(toPage, animated: true)
+            } else {
+                navigationController.pushViewController(toPage, animated: true)
+            }
+        }
+    }
+    
+    static public func pushToAndRemoveUntil(toPage: UIViewController, untilPage: UIViewController?) {
+        
+        if let navigationController = topViewController()?.navigationController {
+            if (untilPage == nil) {
+                navigationController.pushViewController(toPage, animated: true)
+                return
+            }
+            if (navigationController.viewControllers.contains(untilPage!)) {
+                navigationController.popToViewController(untilPage!, animated: false)
+                navigationController.pushViewController(toPage, animated: true)
+            } else {
+                navigationController.dismiss(animated: false) {
+                    pushToAndRemoveUntil(toPage: toPage, untilPage: untilPage)
+                }
+            }
+        }
+    }
+    
+    static func dismissOrPop(animated: Bool) {
+        let viewController = topViewController()
+        if let navigationController = viewController?.navigationController {
+            if navigationController.viewControllers.first != viewController {
+                // 如果视图控制器不是导航堆栈中的根视图控制器，则执行 pop 操作
+                navigationController.popViewController(animated: animated)
+            } else {
+                // 如果是根视图控制器，则执行 dismiss 操作
+                viewController?.dismiss(animated: animated, completion: nil)
+            }
+        } else if viewController?.presentingViewController != nil {
+            // 如果视图控制器是通过 present 呈现的，则执行 dismiss 操作
+            viewController?.dismiss(animated: animated, completion: nil)
+        } else {
+            // 没有导航控制器或呈现控制器，可能是根视图控制器
+            print("This view controller cannot be dismissed or popped")
+        }
+    }
+    
+    /// 获取顶部控制器 无要求
+    public static func topViewController() -> UIViewController? {
+        return FMNavigatorObserver.topViewController()
+    }
+
+}
---
+++ ios/Classes/navigator/FMNavigatorObserver.swift
@@ -0,0 +1,130 @@
+//
+//  FMNavigatorObserver.swift
+//  flutter_meteor
+//
+//  Created by itbox_djx on 2024/7/22.
+//
+
+import UIKit
+
+public class FMNavigatorObserver: NSObject, UINavigationControllerDelegate {
+    public static let shared = FMNavigatorObserver()
+    private override init() { super.init() }
+    
+    private var viewControllerStack: [UIViewController] = []
+    
+    public var routeStack: [UIViewController] {
+        get {
+            return viewControllerStack
+        }
+    }
+    
+    public func startMonitoring() {
+        if let rootViewController = UIApplication.shared.windows.first?.rootViewController {
+            setupViewController(rootViewController)
+        }
+    }
+    
+    private func setupViewController(_ viewController: UIViewController) {
+        if let navigationController = viewController as? UINavigationController {
+            navigationController.delegate = self
+            viewControllerStack.append(contentsOf: navigationController.viewControllers)
+        } else {
+            viewControllerStack.append(viewController)
+        }
+        
+        viewController.children.forEach { setupViewController($0) }
+        if let presentedViewController = viewController.presentedViewController {
+            setupViewController(presentedViewController)
+        }
+    }
+    
+    public func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
+        updateViewControllerStack()
+    }
+    
+    public func updateViewControllerStack() {
+        viewControllerStack.removeAll()
+        if let rootViewController = UIApplication.shared.windows.first?.rootViewController {
+            collectViewControllers(from: rootViewController)
+        }
+        printCurrentViewControllerStack()
+    }
+    
+    private func collectViewControllers(from viewController: UIViewController) {
+
+        if let navigationController = viewController as? UINavigationController {
+            navigationController.delegate = self
+            for vc in navigationController.viewControllers {
+                if !viewControllerStack.contains(vc) {
+                    viewControllerStack.append(vc)
+                }
+            }
+        } else {
+            if !viewControllerStack.contains(viewController) {
+                viewControllerStack.append(viewController)
+            }
+        }
+        viewController.children.forEach { collectViewControllers(from: $0) }
+        if let presentedViewController = viewController.presentedViewController {
+            collectViewControllers(from: presentedViewController)
+        }
+    }
+    
+    func printCurrentViewControllerStack() {
+        let titles = viewControllerStack.map { $0.routeName ?? "\(type(of: $0))" }
+        print("Current View Controllers Stack: \(titles)")
+        FlutterMeteorRouter.routeNameStack { ret in
+            print("Current View routeNameStack: \(String(describing: ret))")
+        }
+    }
+    
+    /// 获取顶部控制器
+    public static func rootViewController() -> UIViewController? {
+        var window = UIApplication.shared.keyWindow
+        // 是否为当前显示的window
+        if ((window?.windowLevel.rawValue) != 0) {
+            let windows = UIApplication.shared.windows
+            for  windowTemp in windows{
+                if windowTemp.windowLevel.rawValue == 0{
+                    window = windowTemp
+                    break
+                }
+            }
+        }
+
+        let vc = window?.rootViewController
+        return vc
+    }
+    
+    /// 获取顶部控制器 无要求
+    public static func topViewController() -> UIViewController? {
+        let vc = rootViewController()
+        return getTopVC(withCurrentVC: vc)
+    }
+    
+    public static func getTopVC(withCurrentVC VC: UIViewController?) -> UIViewController? {
+        guard let currentVC = VC else {
+            return nil
+        }
+        
+        if let presentedVC = currentVC.presentedViewController {
+            // Modal出来的控制器
+            return getTopVC(withCurrentVC: presentedVC)
+        } else if let tabVC = currentVC as? UITabBarController {
+            // tabBar 的根控制器
+            if let selectedVC = tabVC.selectedViewController {
+                return getTopVC(withCurrentVC: selectedVC)
+            }
+            return tabVC
+        } else if let navVC = currentVC as? UINavigationController {
+            // 控制器是导航控制器
+            return getTopVC(withCurrentVC: navVC.visibleViewController)
+        } else {
+            // 返回顶层控制器
+            return currentVC
+        }
+    }
+}
+
+
