3ae31545a644e2032cb232566c14e5e26f3cc503
dongjianxiong <dongjianxiong@itbox.cn>
2024-08-21 18:43:48 +0800

    feat: 修改文件夹名称

--- lib/flutter_meteor.dart
+++ lib/flutter_meteor.dart
@@ -5,5 +5,5 @@ export 'engine/engine.dart';
 export 'engine/entry_args.dart';
 export 'event_bus/meteor_event_bus.dart';
 export 'navigator/navigator.dart';
-export 'state/global_shared_state.dart';
-export 'state/global_shared_state_mixin.dart';
+export 'shared_state/global_shared_state.dart';
+export 'shared_state/global_shared_state_mixin.dart';
